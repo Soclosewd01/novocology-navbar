86b0b558d63e59b314220a9678f2303627295bf4
soCloseDHK <soclose2021dhaka01@gmail.com>
2025-04-24 22:42:57 +0600

    navbar done

--- novo.js
+++ novo.js
@@ -1,49 +1,27 @@
-
 const collapseEl = document.getElementById('smartCollapse');
 const wrapper = document.getElementById('dropdown-wrapper');
+const bellEl = document.querySelector('.notification-active');
 
-// Adjust alignment when collapse is shown
 collapseEl.addEventListener('show.bs.collapse', () => {
   const rect = wrapper.getBoundingClientRect();
   const dropdown = collapseEl;
 
-  // Get viewport width
   const vw = window.innerWidth;
 
-  // Default styles
   dropdown.style.left = 'auto';
   dropdown.style.right = 'auto';
 
-  // Check space on the right
+  // Checking the space if there is any in the left or right.
   const spaceRight = vw - rect.right;
   const spaceLeft = rect.left;
 
   if (spaceRight < 220 && spaceLeft > 220) {
-    // Not enough room on right, but enough on left → align left
     dropdown.style.right = '0';
   } else {
-    // Align right (default)
     dropdown.style.left = '0';
   }
 });
 
-
-
-// const searchWrapper = document.getElementById('searchWrapper');
-// const searchField = document.getElementById('searchField');
-// const closeBtn = document.getElementById('closeBtn');
-// const crossButton = document.getElementById('#cross-button');
-
-// searchField.addEventListener('focus', () => {
-//   searchWrapper.classList.add('expanded');
-//   crossButton.classList.add('d-block');
-// });
-
-// closeBtn.addEventListener('click', () => {
-//   searchWrapper.classList.remove('expanded');
-//   searchField.value = '';
-  
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
   const searchWrapper = document.getElementById('searchWrapper');
@@ -71,4 +49,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+
+// Notification badge js
+// To toggle notification bell there is a  .notification-deactive class.   
+
 
